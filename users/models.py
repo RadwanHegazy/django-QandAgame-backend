3d15307e22e280a3c6550e6997cbba1c82217be7
RadwanHegazy <radwangaber22@gmail.com>
2024-09-23 11:40:37 +0300

    innit server & done auth

---
+++ users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserObjects (BaseUserManager) :
+    def create_user (self, password, **fields) : 
+        user = self.model(**fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, **fields) :
+        fields['is_staff'] = True
+        fields['is_superuser'] = True 
+        return self.create_user(**fields)
+    
+
+class User (AbstractUser) : 
+    objects = UserObjects()
+    username = None
+    first_name = None
+    last_name = None
+
+    full_name = models.CharField(max_length=225)
+    email = models.EmailField(unique=True)
+    picture = models.ImageField(upload_to='user-pics/', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name','picture']
+
+    def __str__(self) -> str:
+        return self.full_name
+    
