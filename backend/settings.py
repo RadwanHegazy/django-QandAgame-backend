78d8ab09f614d6a2ce84b1844a7acb3b910a427d
RadwanHegazy <radwangaber22@gmail.com>
2024-09-24 14:56:04 +0300

    update token establish time

--- backend/settings.py
+++ backend/settings.py
@@ -156,3 +156,11 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
+
+from datetime import timedelta
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
